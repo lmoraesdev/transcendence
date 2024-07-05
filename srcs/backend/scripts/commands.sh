969e043a53fa6e7c1df689ca83bcc3ae213cb467
Erick-Kaik <erick.kaik2002@gmail.com>
2024-07-04 22:35:45 -0300

    Atualização da estrutura

--- srcs/backend/scripts/commands.sh
+++ srcs/backend/scripts/commands.sh
@@ -2,16 +2,26 @@
 
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
-echo 'entrou'
+
+# Esperar até que o PostgreSQL esteja pronto
+echo 'Entrou no shell script'
 while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-  echo "🟡 Waiting for Postgres Database Startup ($POSTGRES_HOST:$POSTGRES_PORT) ..."
+  echo "🟡 Aguardando Inicialização do Banco de Dados PostgreSQL ($POSTGRES_HOST:$POSTGRES_PORT) ..."
   sleep 2
 done
 
-echo "✅ Postgres Database Started Successfully ($POSTGRES_HOST:$POSTGRES_PORT)"
+echo "✅ Banco de Dados PostgreSQL Iniciado com Sucesso ($POSTGRES_HOST:$POSTGRES_PORT)"
 
 # Comandos do Django
-python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+
+# Iniciar Uvicorn com Gunicorn
+echo "🚀 Iniciando servidor Uvicorn com Gunicorn..."
+/venv/bin/gunicorn -w 4 -k uvicorn.workers.UvicornWorker transcendence.asgi:application --bind 0.0.0.0:8000
+
+# Ou se preferir, diretamente com Uvicorn sem Gunicorn
+# echo "🚀 Iniciando servidor Uvicorn..."
+# /venv/bin/uvicorn web.asgi:application --host 0.0.0.0 --port 8000
+
