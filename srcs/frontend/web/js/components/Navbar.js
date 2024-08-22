32770e3f15ba073a8ee5769d7c31241d0dfe68a9
MelTami <melissa.vavassori@gmail.com>
2024-08-21 22:10:17 -0300

    add: new navbar

--- srcs/frontend/web/js/components/Navbar.js
+++ srcs/frontend/web/js/components/Navbar.js
@@ -3,42 +3,31 @@ import fetching from "../helpers/fetching.js";
 const Navbar = async () => {
   const navbarHTML = `
     <template id="navbar-template">
-      <nav class="navbar navbar-expand-lg bg-white m-4 border border-2 border-dark rounded-5 p-0 align-items-center">
-        <div class="container-fluid">
-          <a class="navbar-brand fs-6 px-1 py-0" href="#">
-            <img 
-              id="profile-photo"
-              src="https://static.vecteezy.com/system/resources/previews/047/589/492/non_2x/profile-photo-logo-sign-outline-vector.jpg"
-              alt="user photo"
-              class="profile-photo mt-2 d-inline-block align-text-top rounded-circle border border-3"
-              style="border-color: #F3C00C !important;"
-            />
-            <p id="profile-name" class="text-light text-center"></p>
-          </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center align-items-center w-100 gap-1">
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/game-modality/">Game</a></li>
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/statistics/">Statistics</a></li>
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/leaderboard/">Leaderboard</a></li>
-              <!--<li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/tournaments/">Tournaments</a></li>-->
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/guilds/">Guilds</a></li>
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/profile/">Profile</a></li>
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0" href="/settings/">Settings</a></li>
-              <li class="nav-item"><a class="link nav-link fs-5 px-1 py-0 loggout" id="logout">Loggout</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+     <nav class="navbar">
+      <div>
+        <img src="./web/images/logo_pong.png" alt="logo pong"/>
+      </div>
+      <div class="navbar-items">
+        <ul class="navbar-items">
+          <li><button class="navbar-button">Home</button></li>
+          <li><button class="navbar-button">Statistics</button></li>
+          <li><button class="navbar-button">Leaderboard</button></li>
+          <li><button class="navbar-button">Tournament</button></li>
+          <li><button class="navbar-button">Guilds</button></li>
+        </ul>
+      </div>
+      <div class="navbar-perfil">
+        <p>Nome</p>
+        <img src="./web/images/pngegg.png" alt="Foto do perfil" width="100px" height="100px"/>
+      </div>
+    </nav>
     </template>
   `;
 
   if (!document.querySelector('#navbar-template')) {
     const templateContainer = document.createElement('div');
     templateContainer.innerHTML = navbarHTML;
-    document.body.appendChild(templateContainer);
+    document.body.appendChild(templateContainer);<li><button></button></li>
   }
 
   const template = document.getElementById("navbar-template");
