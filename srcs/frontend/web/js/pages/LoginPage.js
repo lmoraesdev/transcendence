9aa76ccfda9d405df3a79dc4a51b062109dfe08f
lucperei <lucperei@student.42sp.org.br>
2024-08-29 02:48:39 -0300

    adding sensitivity to login

--- srcs/frontend/web/js/pages/LoginPage.js
+++ srcs/frontend/web/js/pages/LoginPage.js
@@ -23,9 +23,9 @@ const LoginPage = async () => {
           </div>
           <div class="login-content-buttons d-grid p-5 gap-2 mx-auto"></div>
         </div>
-        <div class="login-img p-4 col m-2">
+        <div class="login-img p-4 col d-flex justify-content-center">
           <img 
-            alt="Demo Game Pong" 
+            alt="Animation of the Pong game with paddle movements and ball bouncing" 
             src="https://cdn.dribbble.com/users/2092880/screenshots/6426030/pong_1.gif" 
             class="img-fluid rounded-5 p-4 object-fit-cover" 
           />
@@ -50,20 +50,24 @@ const LoginPage = async () => {
 
   const button42 = ButtonLogin({
     label: 'Login with 42',
-    iconSVG: '<img src="/web/images/42_logo.svg" class="img-fluid" style="width:45px; height: 30px;">',
+    iconSVG: '<img src="/web/images/42_logo.svg" class="img-fluid" style="width:45px; height: 30px;" aria-hidden="true">',
     customClasses: ['mb-2'],
     link: `https://${window.ft_transcendence_host}/authentication/intra/`,
   });
+  button42.setAttribute('aria-label', 'Login with 42');
 
   const buttonGoogle = ButtonLogin({
     label: 'Login with Google',
-    iconSVG: '<img src="/web/images/googlelogo.png" class="img-fluid" style="width:35px; height: 30px;">',
+    iconSVG: '<img src="/web/images/googlelogo.png" class="img-fluid" style="width:35px; height: 30px;" aria-hidden="true">',
     customClasses: [],
     link: `https://${window.ft_transcendence_host}/authentication/google/`,
   });
+  buttonGoogle.setAttribute('aria-label', 'Login with Google');
 
   buttonsContainer.appendChild(button42);
   buttonsContainer.appendChild(buttonGoogle);
+
+  setFocus(button42);
 };
 
 export default LoginPage;
